9b23780754ed87ee30cc013c2b726bbe19598cab
zhaozhong <1260481758@qq.com>
2019-06-20 19:37:59 +0800

    create static-server-fe

---
+++ static-server-fe/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd'
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
